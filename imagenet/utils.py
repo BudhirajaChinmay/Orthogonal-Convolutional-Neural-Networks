dea79870118422cbe22e1f347ba9701fc14b849e
Chinmay <chinmay17274@iiitd.ac.in>
2020-08-29 02:08:01 +0530

    Data Loader Modified

--- imagenet/utils.py
+++ imagenet/utils.py
@@ -224,16 +224,15 @@ def get_training_dataloader(mean, std, batch_size=16, num_workers=2, shuffle=Tru
 
     transform_train = transforms.Compose([
         #transforms.ToPILImage(),
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(15),
+        # transforms.RandomCrop(32, padding=4),
+        # transforms.RandomHorizontalFlip(),
+        # transforms.RandomRotation(15),
         transforms.ToTensor(),
         transforms.Normalize(mean, std)
     ])
     #cifar100_training = CIFAR100Train(path, transform=transform_train)
     
-    # cifar100_training = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
-    cifar100_training = torchvision.datasets.CIFAR100(root = './data', train = True, download = True)
+    cifar100_training = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
     if perc < 1.0:
         idx = np.load("misc/cifar100_"+str(perc)+".npy")
         cifar100_training = torch.utils.data.Subset(cifar100_training, idx)
@@ -256,16 +255,15 @@ def get_training_dataloaderIMAGENET(mean, std, batch_size=16, num_workers=2, shu
 
     transform_train = transforms.Compose([
         #transforms.ToPILImage(),
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(15),
+        # transforms.RandomCrop(32, padding=4),
+        # transforms.RandomHorizontalFlip(),
+        # transforms.RandomRotation(15),
         transforms.ToTensor(),
         transforms.Normalize(mean, std)
     ])
     #cifar100_training = CIFAR100Train(path, transform=transform_train)
     
-    # cifar100_training = torchvision.datasets.ImageNet(root='/home/peterwg/dataset/ILSVRC2012', split='train', download=False, transform=transform_train)
-    cifar100_training = torchvision.datasets.ImageNet(root='/home/peterwg/dataset/ILSVRC2012', split='train', download=False)
+    cifar100_training = torchvision.datasets.ImageNet(root='/home/peterwg/dataset/ILSVRC2012', split='train', download=False, transform=transform_train)
 
     st()
     if perc < 1.0:
